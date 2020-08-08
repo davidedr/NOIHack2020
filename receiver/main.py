4915b49992baca3c70b6b927273134debbe75703
Yong Hoon Shin <zcabyhs@ucl.ac.uk>
2020-08-08 14:55:23 +0900

    Image reassemble

--- receiver/main.py
+++ receiver/main.py
@@ -2,6 +2,8 @@ from network import LoRa
 import socket
 import machine
 import time
+import numpy as np
+from PIL import Image
 
 # initialise LoRa in LORA mode
 # Please pick the region that matches where you are using the device:
@@ -14,6 +16,9 @@ lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868, sf=7)
 
 # create a raw LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+image = []
+
 
 def receive_and_send_ack():
 
@@ -33,16 +38,31 @@ def receive_and_send_ack():
         # wait a random amount of time
         time.sleep(0.1)
 
-def listen_for_image_parts(num_chunks, chunks_received=[]):
-    image = []
-    for i in range(num_chunks * 2 * 10):
-        data = s.recv(300)
 
+def assembleImage():
+    # image is in the following format:
+    '''
+        [
+            [[8 bits r, 8 bits g, 8 bits b], [8r,8g,8b]]... one arr for each pixel
+            ...
+            ..
+            .
+        ]
+    '''
+    npa = np.asarray(image)
+    img = Image.fromarray(npa, 'RGB')
+    img.save('received.png')
+
+
+def listen_for_image_parts(num_chunks, chunks_received=[]):
+    for i in range(num_chunks * 2 * 10):
+        data = s.recv(300)  # why 300 for choice of buf size?
         data = data.decode('utf-8')
 
         chunk = []
         if len(data) > 0:
             try:
+                # number_of_chunk is the index of chunk
                 number_of_chunk = int(data[:8], 2)
                 chunks_received.append(number_of_chunk)
 
@@ -57,22 +77,28 @@ def listen_for_image_parts(num_chunks, chunks_received=[]):
             if chunk not in image:
                 image.append(chunk)
 
-            print(len(image))
+        if(len(image) == num_chunks):
+            # received all the chunks to form the image
+            assembleImage()
 
         time.sleep(0.5)
 
     return list(set(chunks_received))
 
+
 def retry_wrong_image_parts(num_chunks, chunks_received):
-    wrong_parts = [i for i in range(len(num_chunks)) if i not in chunks_received]
+    wrong_parts = [i for i in range(
+        len(num_chunks)) if i not in chunks_received]
     print(wrong_parts)
 
     if len(wrong_parts) > 0:
-        wrong_parts_encoded = "".join(["{0:b}".format(part) for part in wrong_parts])
+        wrong_parts_encoded = "".join(
+            ["{0:b}".format(part) for part in wrong_parts])
         s.send(wrong_parts_encoded)
         listen_for_image_parts(num_chunks, chunks_received=chunks_received)
     else:
         s.send("OK")
+
 
 num_chunks = receive_and_send_ack()
 chunks_received = listen_for_image_parts(num_chunks)
